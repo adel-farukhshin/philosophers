8d611b2d229e4ac1f483228cd32d955380ea2c48
adelfar <afarukhshin@gmail.com>
2022-06-04 18:38:46 +0300

    fix the smart_sleep: change the now and start time

--- src/philo_functions.c
+++ src/philo_functions.c
@@ -22,21 +22,27 @@
 void		smart_sleep(long long time, t_philo *philo)
 {
 	long long i;
+	long long a;
 	
 	i = timestamp();
+	// print_action(philo, "a ts");
 	while (!(philo->data->is_to_die))
 	{
-		if (i - timestamp() >= time)
+		a = timestamp();
+		if (a - i >= time)
 			break ;
 		usleep(50);
+		// print_action(philo, "ss: cycle");
+		// printf("n %llu st %llu min %llu t %llu\n", a, i, a - i, time);
 	}
+	// print_action(philo, "ss: at the end ");
 }
 
 
 void	ph_sleep(t_philo *philo)
 {
 	print_action(philo, "is sleeping");
-	smart_sleep(philo->data->to_sleep * 1000, philo);
+	smart_sleep(philo->data->to_sleep, philo);
 }
 
 void	ph_think(t_philo *philo)
@@ -68,11 +74,12 @@ int	left(int n, int ph_num)
 void	ph_eat(t_philo *philo)
 {
 	pthread_mutex_lock(philo->last_m);
+	print_action(philo, "is eating");
 	philo->last_meal = timestamp();
 	pthread_mutex_unlock(philo->last_m);
-	print_action(philo, "is eating");
-	smart_sleep(philo->data->to_eat * 1000, philo);
-
+	// print_action(philo, "a l_m");
+	smart_sleep(philo->data->to_eat, philo);
+	// print_action(philo, "a ss");
 }
 
 
@@ -80,8 +87,8 @@ void	*philosopher(void *data)
 {
 	t_philo *philo = data;
 
-	// if (philo->index % 2 == 0)
-	// 	usleep(2500);
+	if (philo->index % 2 == 0)
+		usleep(2500);
 
 	// i = 0;
 	while (!(philo->data->is_to_die))
@@ -94,16 +101,23 @@ void	*philosopher(void *data)
 		pthread_mutex_lock(MAX(philo->fork_r, philo->fork_l));
 		print_action(philo, "has taken a fork");
 
+		// print_action(philo, "aaaa");
 		// Eating
 		ph_eat(philo);
-		pthread_mutex_unlock(MAX(philo->fork_r, philo->fork_l));
+		// print_action(philo, "ab");
+		pthread_mutex_unlock(MAX(philo->fork_r, philo->fork_l)); 
+		// print_action(philo, "ac");
 		pthread_mutex_unlock(MIN(philo->fork_r, philo->fork_l));
+		
+		// print_action(philo, "bbb");
 
 		// Sleep
 		ph_sleep(philo);
 
+		// print_action(philo, "cc");
 		// Think
 		ph_think(philo);
+		// print_action(philo, "ddd");
 	}
 	return (0);
 }
