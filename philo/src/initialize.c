42c5165e246cd1bfed2b1367c4308202b71aecb5
Butcher Sarai <bsarai@at-k3.msk.21-school.ru>
2022-06-05 12:57:34 +0300

    norm: initialize.c

--- philo/src/initialize.c
+++ philo/src/initialize.c
@@ -37,68 +37,7 @@ int	init_philos(t_philos *philos)
 	return (0);
 }
 
-// void	mutex_delete(pthread_mutex_t *mutex, int nb)
-// {
-// 	while (nb > -1)
-// 	{
-// 		pthread_mutex_destroy(mutex + nb);
-// 		nb--;
-// 	}
-// }
-
-// int mutex_init(pthread_mutex_t **type, int nb)
-// {
-// 	int	i;
-// 	pthread_mutex_t *mutex;
-
-// 	i = 0;
-
-// 	mutex = malloc(sizeof(pthread_mutex_t) * nb);
-// 	if (!mutex)
-// 		return (1);	
-// 	while (i < nb)
-// 	{
-// 		if (pthread_mutex_init(mutex + i, NULL))
-// 		{
-// 			mutex_delete(mutex, i - 1);
-// 			return (2);
-// 		}
-// 		i++;
-// 	}
-// 	*type = mutex;
-// 	return (0);
-// }
-
-// void	all_mutex_delete(t_philos *philos, int mode)
-// {
-// 	if (mode > 0)
-// 		mutex_delete(philos->forks, philos->ph_num - 1);
-// 	if (mode > 1)
-// 		mutex_delete(philos->last_mutexes, philos->ph_num - 1);
-// 	if (mode > 2)
-// 		mutex_delete(philos->data.out_m, 1 - 1);
-// }
-
-// int	all_mutex_init(t_philos *philos)
-// {
-// 	if (mutex_init(&(philos->forks), philos->ph_num))
-// 		return (1);
-// 	if (mutex_init(&(philos->last_mutexes), philos->ph_num))
-// 	{
-// 		all_mutex_delete(philos, 1);
-// 		return (2);
-// 	}
-// 	if (mutex_init(&(philos->data.out_m), 1))
-// 	{
-// 		all_mutex_delete(philos, 2);
-// 		return (3);
-// 	}
-// 	return (0);
-// }
-
-int		initialize(t_philos *philos, int ac, char **av)
-// number_of_philosophers time_to_die time_to_eat time_to_sleep
-// [number_of_times_each_philosopher_must_eat]
+int	initialize(t_philos *philos, int ac, char **av)
 {
 	philos->ph_num = ft_atoi(av[1]);
 	philos->data.ph_num = ft_atoi(av[1]);
@@ -111,7 +50,6 @@ int		initialize(t_philos *philos, int ac, char **av)
 		philos->times_to_eat = -1;
 	philos->data.is_all_ate = 0;
 	philos->data.is_to_die = 0;
-
 	if (all_mutex_init(philos))
 		return (1);
 	if (init_philos(philos))
@@ -127,7 +65,6 @@ int		initialize(t_philos *philos, int ac, char **av)
 // int	main()
 // {
 // 	t_philos	philos;
-	
 // 	initialize(&philos);
 // 	int	i = 0;
 
