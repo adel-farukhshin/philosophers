922a57949f0282a880364b1353a7e863808d020f
Butcher Sarai <bsarai@at-k3.msk.21-school.ru>
2022-06-05 13:09:50 +0300

    refactor: add thread_create; change the is_died cycle

--- philo/src/launch.c
+++ philo/src/launch.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   launch.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsarai <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/05 13:01:01 by bsarai            #+#    #+#             */
+/*   Updated: 2022/06/05 13:01:03 by bsarai           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "philosophers.h"
 #include <stdlib.h>
@@ -28,25 +39,20 @@ int	is_all_ate(t_philos *philos)
 int	is_died(t_philos *philos)
 {
 	int	i;
-	// long long	now;
-	
+
 	i = 0;
-	while (i < philos->ph_num && !(philos->data.is_to_die) && !(philos->data.is_all_ate))
+	while (i < philos->ph_num
+		&& !(philos->data.is_to_die) && !(philos->data.is_all_ate))
 	{
-		// now = timestamp();
-		pthread_mutex_lock(((philos->ph_arr) + i)->last_m); // one last_m
-		if (timedif(philos->ph_arr[i].last_meal, timestamp()) >= philos->data.to_die)
+		pthread_mutex_lock(((philos->ph_arr) + i)->last_m);
+		if (timedif(philos->ph_arr[i].last_meal
+				, timestamp()) >= philos->data.to_die)
 		{
-			// printf("last %llu, now %llu, %d\n", philos->ph_arr[i].last_meal, now, philos->data.is_to_die);
 			pthread_mutex_unlock(((philos->ph_arr) + i)->last_m);
 			print_action(philos->ph_arr + i, "died");
 			philos->data.is_to_die = 1;
-			// usleep(100); // not commented
-			return (1); // break
-			// break ;
+			return (1);
 		}
-		// if (philos->data.is_to_die)
-		// 	break ;
 		pthread_mutex_unlock(((philos->ph_arr) + i)->last_m);
 		if (is_all_ate(philos))
 			return (2);
@@ -57,7 +63,7 @@ int	is_died(t_philos *philos)
 
 int	thread_delete(pthread_t *t, int nb)
 {
-	int error;
+	int	error;
 
 	error = 0;
 	while (nb > -1)
@@ -69,34 +75,44 @@ int	thread_delete(pthread_t *t, int nb)
 	return (error);
 }
 
-int		launch(t_philos *philos)
+int	thread_create(t_philos *philos, pthread_t	*t)
+{
+	int	i;
+
+	i = 0;
+	while (i < philos->ph_num)
+	{
+		if (pthread_create((t + i)
+				, NULL, philosopher, (philos->ph_arr + i)))
+		{
+			if (thread_delete(t, i - 1))
+				return (1);
+		}
+		(philos->ph_arr + i)->last_meal = timestamp();
+		i++;
+	}
+	return (0);
+}
+
+int	launch(t_philos *philos)
 {	
-	// Threads Initialization
-	pthread_t 	*t;
+	pthread_t	*t;
 	int			i;
 	int			error;
-	
+
 	t = malloc(sizeof(pthread_t) * philos->ph_num);
 	if (!t)
 		return (1);
 	i = 0;
 	philos->data.start = timestamp();
-	// printf("%llu\n", philos->data.start);
-	while (i < philos->ph_num)
+	if (thread_create(philos, t))
 	{
-		if(pthread_create((t + i), NULL, philosopher, (philos->ph_arr + i)))
-		{
-			if (thread_delete(t, i - 1))
-			{
-				free (t);
-				return (2);
-			}
-		}
-		(philos->ph_arr + i)->last_meal = timestamp();
-		// printf("%lld\n", (philos->ph_arr + i)->last_meal);
-		i++;
+		free (t);
+		return (2);
 	}
-	while (!is_died(philos));
+	while (1)
+		if (is_died(philos))
+			break ;
 	error = thread_delete(t, philos->ph_num - 1);
 	free(t);
 	return (error);
