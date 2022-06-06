c0609d1fe9a7278d4474babcd31d03a72009fefc
Butcher Sarai <bsarai@at-p4.msk.21-school.ru>
2022-06-06 17:54:31 +0300

    delete print_action from to_stop

--- philo_bonus/src/util_functions.c
+++ philo_bonus/src/util_functions.c
@@ -50,19 +50,4 @@ long long	timedif(long long past, long long pres)
 {
 	return (pres - past);
 }
-/*
-void	print_action(t_philo *philo, char *msg)
-{
-	// lock the writing mutex
-	pthread_mutex_lock(philo->data->out_m);
-	if (!(philo->data->is_to_die))
-	{
-		printf("%llu ", timedif(philo->data->start, timestamp()));
-		printf("%d ", philo->index);
-		printf("%s\n", msg);
-	}
-	// unlock the writing mutex
-	pthread_mutex_unlock(philo->data->out_m);
-	return ;
-}
-*/
+
