d8ccad1a55564e52894b4fb8796a30bea0768e3d
Butcher Sarai <bsarai@at-k2.msk.21-school.ru>
2022-06-20 14:48:46 +0300

    rename philosophers to main; spread philosopher functions between philosopher and philo_functions file

--- philo_bonus/inc/philosophers.h
+++ philo_bonus/inc/philosophers.h
@@ -82,6 +82,7 @@ int			ft_atoi(const char *str);
 
 int			launch(t_philos *philos);
 int			philosopher(t_philo *philo);
+void		ph_routine(t_philo *philo);
 void		smart_sleep(t_philo *philo, long long time);
 long long	timestamp(void);
 long long	timedif(long long last, long long next);
--- philo_bonus/src/philo_functions.c
+++ philo_bonus/src/philo_functions.c
@@ -16,66 +16,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-static void	ph_routine(t_philo *philo);
-static void	*to_stop(void *data);
-static int	check_eaten(t_philo *philo);
-static int	check_died(t_philo *philo);
-static int	change_eaten(t_philo *philo);
-
-int	philosopher(t_philo *philo)
-{
-	pthread_t	t;
-	
-	if (add_sem(philo))
-		exit (1);
-	if (pthread_create(&t, NULL, to_stop, philo))
-	{
-		remove_sem(philo, 4);
-		exit (1);
-	}
-	if (philo->index % 2 == 0)
-		smart_sleep(philo, philo->to_eat / 2);
-	while (1)
-	{
-		ph_routine(philo);
-		if (check_eaten(philo))
-			break ;
-		if (check_died(philo))
-			break;
-	}
-	pthread_join(t, NULL);
-	exit (0);
-}
-
-static void	*to_stop(void *data)
-{
-	t_philo	*philo;
-
-	philo = data;
-	while (1)
-	{
-		if (change_eaten(philo))
-			break;
-		sem_wait(philo->last_s);
-		if (timestamp() - philo->last_meal >= philo->to_die)
-		{
-			sem_wait(philo->die_s);
-			philo->is_to_die = 1;
-			sem_post(philo->die_s);
-			sem_wait(philo->out);
-			printf("%llu %d is_died\n", timestamp() - philo->start, philo->index);
-			break ;
-		}
-		sem_post(philo->last_s);
-	}
-	remove_sem(philo, 4);
-	if (philo->is_to_die)
-		exit (1);
-	else
-		exit (0);
-}
-
-static void	ph_routine(t_philo *philo)
+void	ph_routine(t_philo *philo)
 {
 	sem_wait(philo->fork);
 	print_action(philo, "has taken a 1_fork");
@@ -94,43 +35,4 @@ static void	ph_routine(t_philo *philo)
 	print_action(philo, "is sleeping");
 	smart_sleep(philo, philo->to_sleep);
 	print_action(philo, "is thinking");
-}
-
-static int	check_eaten(t_philo *philo)
-{
-	sem_wait(philo->is_eaten_s);
-	if (philo->is_eaten)
-	{
-		sem_post(philo->is_eaten_s);
-		return (1);
-	}
-	sem_post(philo->is_eaten_s);
-
-	return (0);
-}
-
-static int	check_died(t_philo *philo)
-{
-	sem_wait(philo->die_s);
-	if (philo->is_to_die)
-	{
-		sem_post(philo->die_s);
-		return (1);
-	}
-	sem_post(philo->die_s);
-	return (0);
-}
-
-static int	change_eaten(t_philo *philo)
-{
-	sem_wait(philo->nm_s);
-	if (philo->times_to_eat != -1 && philo->nb_meal == philo->times_to_eat)
-	{
-		sem_wait(philo->is_eaten_s);
-		philo->is_eaten = 1;
-		sem_post(philo->is_eaten_s);
-		return (1);
-	}
-	sem_post(philo->nm_s);
-	return (0);
 }
