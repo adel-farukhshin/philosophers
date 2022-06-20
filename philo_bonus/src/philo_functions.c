c58ddd3d03016c01fc9131894421e61c91d494cd
Butcher Sarai <afarukhshin@gmail.com>
2022-06-20 12:17:20 +0300

    refactor: delete comments and blank lines

--- philo_bonus/src/philo_functions.c
+++ philo_bonus/src/philo_functions.c
@@ -16,68 +16,6 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-
-/*
-
-pthread_mutex_t	*min(pthread_mutex_t *r, pthread_mutex_t *l)
-{
-	if (r < l)
-		return (r);
-	else
-		return (l);
-}
-
-pthread_mutex_t	*max(pthread_mutex_t *r, pthread_mutex_t *l)
-{
-	if (r > l)
-		return (r);
-	else
-		return (l);
-}
-*/
-
-/*
-void	ph_eat(t_philo *philo)
-{
-	pthread_mutex_lock(philo->last_m);
-	print_action(philo, "is eating");
-	philo->last_meal = timestamp();
-	pthread_mutex_unlock(philo->last_m);
-	philo->nb_meal += 1;
-	smart_sleep(philo->data->to_eat, philo);
-}
-
-*/
-
-// void	*philosopher(void *data)
-// {
-// 	t_philo	*philo;
-
-// 	philo = data;
-// 	if (philo->index % 2 == 0)
-// 		usleep(20);
-// 	if (philo->data->ph_num == 1)
-// 	{
-// 		smart_sleep(philo->data->to_die, philo);
-// 		philo->data->is_to_die = 1;
-// 	}
-// 	while (!(philo->data->is_to_die) && !(philo->data->is_all_ate))
-// 	{
-// 		pthread_mutex_lock(min(philo->fork_r, philo->fork_l));
-// 		print_action(philo, "has taken a fork");
-// 		pthread_mutex_lock(max(philo->fork_r, philo->fork_l));
-// 		print_action(philo, "has taken a fork");
-// 		ph_eat(philo);
-// 		pthread_mutex_unlock(max(philo->fork_r, philo->fork_l));
-// 		pthread_mutex_unlock(min(philo->fork_r, philo->fork_l));
-// 		print_action(philo, "is sleeping");
-// 		smart_sleep(philo->data->to_sleep, philo);
-// 		print_action(philo, "is thinking");
-// 	}
-// 	return (0);
-// }
-
-
 void	*to_stop(void *data)
 {
 	t_philo	*philo;
@@ -119,25 +57,18 @@ void	ph_routine(t_philo *philo)
 	print_action(philo, "has taken a 1_fork");
 	sem_wait(philo->fork);
 	print_action(philo, "has taken a 2_fork");
-
 	print_action(philo, "is eating");
 	sem_wait(philo->last_s);
 	philo->last_meal = timestamp();
 	sem_post(philo->last_s);
-
-	
 	smart_sleep(philo, philo->to_eat);
-
 	sem_post(philo->fork);
 	sem_post(philo->fork);
-
 	sem_wait(philo->nm_s);
 	philo->nb_meal++;
 	sem_post(philo->nm_s);
-
 	print_action(philo, "is sleeping");
 	smart_sleep(philo, philo->to_sleep);
-
 	print_action(philo, "is thinking");
 }
 
@@ -147,18 +78,15 @@ int	philosopher(t_philo *philo)
 	
 	if (add_sem(philo))
 		exit (1);
-
 	if (pthread_create(&t, NULL, to_stop, philo))
 	{
 		remove_sem(philo, 4);
 		exit (1);
 	}
-
 	if (philo->index % 2 == 0)
 		smart_sleep(philo, philo->to_eat / 2);
 
 	while (1)
-	// routine
 	{
 		ph_routine(philo);
 
@@ -169,7 +97,6 @@ int	philosopher(t_philo *philo)
 			break ;
 		}
 		sem_post(philo->is_eaten_s);
-
 		sem_wait(philo->die_s);
 		if (philo->is_to_die)
 		{
@@ -178,72 +105,6 @@ int	philosopher(t_philo *philo)
 		}
 		sem_post(philo->die_s);
 	}
-	
 	pthread_join(t, NULL);
-
-	// if (pthread_join(t, NULL)) // error
-	// {
-	// 	printf("a\n"); // delete
-	// 	return (1);
-	// }
-	// else
-	// {
-	// 	printf("b\n");
-	// 	return (0);
-	// }
 	exit (0);
 }
-
-
-
-/*
-int	add_sem(t_philo *philo)
-{
-	char	buf[10];
-
-	name_file("last_", buf, philo->index);
-	sem_unlink(buf);
-	philo->last_s = sem_open(buf, O_CREAT, 0644, 1);
-	if (philo->last_s == SEM_FAILED)
-		return (1);
-	// print_action(philo, "create last"); // delete
-
-	name_file("die_", buf, philo->index);
-	sem_unlink(buf);
-	philo->die_s = sem_open(buf, O_CREAT, 0644, 1);
-	if (philo->die_s == SEM_FAILED)
-	{
-		remove_sem(philo, 1);
-		return (1);
-	}
-	// print_action(philo, "create die"); // delete
-	return (0);
-}
-*/
-
-
-
-
-
-
-/*
-void	remove_sem(t_philo *philo, int mode)
-{
-	char	buf[10];
-
-	if (mode > 0)
-	{
-		sem_close(philo->last_s);
-		name_file("last_", buf, philo->index);
-		sem_unlink(buf);
-	}
-	if (mode > 1)
-	{
-		sem_close(philo->die_s);
-		name_file("die_", buf, philo->index);
-		sem_unlink(buf);
-	}
-}
-*/
-
-
